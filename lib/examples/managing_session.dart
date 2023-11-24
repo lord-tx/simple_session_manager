3c2b07d50c5c47c4895e47b982ecf50770de28c6
Johnpaul M <johnpaul.muoneme@dreamlabs.com.ng>
2023-11-24 16:30:46 +0100

    chore: move test skeleton to respective folder

---
+++ lib/examples/managing_session.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MySessionApp());
+}
+
+class MySessionApp extends StatelessWidget {
+  const MySessionApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,35 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:simple_session_manager/examples/managing_session.dart';
 import 'package:simple_session_manager/simple_session_manager.dart';
 
 void main() {
   testWidgets('Test Session Management', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(SimpleSessionManager(child: const MyApp()));
+    await tester.pumpWidget(const SimpleSessionManager(child: MySessionApp()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    /// Add tests to check if session function is called when needed
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
